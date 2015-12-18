0c4f9a2e0bb4a6d4a3c3bccc964a0049460f4f5b
Rod Chen <ozaki90@gmail.com>
2015-12-18 11:42:24 +0800

    Registering an Application to a URI Scheme

---
+++ UriSchemeTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UriSchemeTest
+{
+    class Program
+    {
+        static string ProcessInput(string s)
+        {
+            // TODO Verify and validate the input 
+            // string as appropriate for your application.
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UriSchemeTest.exe invoked with the following parameters.\r\n");
+            Console.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
+
+            Console.WriteLine("\n\nArguments:\n");
+            foreach (string s in args)
+            {
+                Console.WriteLine("\t" + ProcessInput(s));
+            }
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
